da673b79d1dcf8ba8b1666744fd21bf684a676f6
felix-jwr <felix.rammell@outlook.com>
2024-11-30 16:16:59 +0000

    Fixed issue with the Docker image

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /app/src
-# python3 train.py
-# python3 test.py
 # python3 -u train.py 2>&1 | tee ../results/train_output.log
-python3 -u test.py 2>&1 | tee ../results/test_output.log
+# python3 -u test.py 2>&1 | tee ../results/test_output.log
+python3 temp.py
