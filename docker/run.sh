7d3496463205799a7fc6b5cc5fc2c7f864140beb
Felix <76658412+felix-jwr@users.noreply.github.com>
2025-02-27 10:33:23 +0000

    Updated Docker setup to work on local pc (need to test new image on Hex)

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-# Run the training and testing scripts
-cd /app/src
+cd /workspace/src/
+
 # python3 -u fine_tune.py 2>&1 | tee ../log/gsm8k/finetune/ft-phi-4-bnb-4bit
-python3 -u single_gpu_test.py 2>&1 | tee ../log/gsm8k/test/test-llama-3.2-3B-Instruct-bnb-4bit-CoT
+
+# NOTE: Change --no_cot to --cot to enable COT, similarly for --load_in_4bit
+python3 -u single_gpu_test.py \
+  --model_name "unsloth/Llama-3.1-8B-Instruct-bnb-4bit" \
+  --chat_template "unsloth" \
+  --max_seq_length 2048 \
+  --load_in_4bit \
+  --dataset "openai/gsm8k" \
+  --subset "main" \
+  --split "test" \
+  --no_cot \
+  --n_shot 0 \
+  2>&1 | tee ../log/temp-docker-local-setup
