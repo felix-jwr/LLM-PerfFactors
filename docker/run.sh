7d21349e3121cb15d3ad6b51989f743c7e129243
Felix <76658412+felix-jwr@users.noreply.github.com>
2025-03-24 14:36:58 +0000

    Remove old results files from Git Repo (Declutter)

--- docker/run.sh
+++ docker/run.sh
@@ -4,13 +4,14 @@ cd /workspace/src/
 
 # NOTE: Change --no_cot to --use_cot to enable COT, similarly for --load_in_4bit
 python3 -u test_mmlu.py \
-  --model_name "meta-llama/Llama-3.2-3B-Instruct" \
-  --chat_template "llama" \
+  --model_name "unsloth/gemma-2-9b-it-bnb-4bit" \
+  --chat_template "gemma-2" \
+  --max_seq_length 512 \
   --load_in_4bit \
   --single_gpu \
   --random_seed 42 \
-  --task_names haerae \
+  --task_names gsm8k \
   --no_cot \
   --n_shot 0 \
-  --batch_size 16 \
-  2>&1 | tee ../log/mmlu-haerae/llama-3.2-3b-instruct/test-0shot-nocot-llama-3.2-3b-instruct
+  --batch_size 8 \
+  2>&1 | tee ../log/mmlu-gsm8k/gemma-2-9b-it/test-0shot-nocot-gemma-2-9b-it
