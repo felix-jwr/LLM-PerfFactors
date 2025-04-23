d2273d849850344260a3b36bc7e6f7f2549a52bd
Felix <76658412+felix-jwr@users.noreply.github.com>
2025-04-23 09:39:43 +0100

    Update run script for Distilled Qwen 32B runs on GSM8k

--- docker/run.sh
+++ docker/run.sh
@@ -3,211 +3,58 @@
 cd /workspace/src/
 
 # NOTE: Change --no_cot to --use_cot to enable COT, similarly for --load_in_4bit
-# python3 -u test_mmlu.py \
-#   --model_name "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B" \
-#   --chat_template "deepseek" \
-#   --max_seq_length 512 \
-#   --load_in_4bit \
-#   --single_gpu \
-#   --random_seed 42 \
-#   --task_names haerae_deepseek \
-#   --no_cot \
-#   --n_shot 0 \
-#   --batch_size 64 \
-#   2>&1 | tee ../log/haerae-deepseek/test-haerae-0shot-nocot-Distill-Qwen-1.5B.log
-
-# python3 -u test_mmlu.py \
-#   --model_name "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B" \
-#   --chat_template "deepseek" \
-#   --max_seq_length 512 \
-#   --load_in_4bit \
-#   --single_gpu \
-#   --random_seed 42 \
-#   --task_names haerae_deepseek_cot \
-#   --use_cot \
-#   --n_shot 0 \
-#   --batch_size 64 \
-#   2>&1 | tee ../log/haerae-deepseek/test-haerae-0shot-cot-Distill-Qwen-1.5B.log
-
-# python3 -u test_mmlu.py \
-#   --model_name "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B" \
-#   --chat_template "deepseek" \
-#   --max_seq_length 512 \
-#   --load_in_4bit \
-#   --single_gpu \
-#   --random_seed 42 \
-#   --task_names haerae_deepseek \
-#   --no_cot \
-#   --n_shot 8 \
-#   --batch_size 16 \
-#   2>&1 | tee ../log/haerae-deepseek/test-haerae-8shot-nocot-Distill-Qwen-1.5B.log
-
-python3 -u test_mmlu.py \
-  --model_name "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B" \
+python3 -u test_gsm.py \
+  --model_name "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B" \
   --chat_template "deepseek" \
   --max_seq_length 512 \
   --load_in_4bit \
-  --single_gpu \
   --random_seed 42 \
-  --task_names haerae_deepseek_cot \
-  --use_cot \
-  --n_shot 8 \
-  --batch_size 16 \
-  2>&1 | tee ../log/haerae-deepseek/test-haerae-8shot-cot-Distill-Qwen-1.5B.log
-
-# python3 -u test_mmlu.py \
-#   --model_name "deepseek-ai/DeepSeek-R1-Distill-Llama-8B" \
-#   --chat_template "deepseek" \
-#   --max_seq_length 512 \
-#   --load_in_4bit \
-#   --single_gpu \
-#   --random_seed 42 \
-#   --task_names haerae_deepseek \
-#   --no_cot \
-#   --n_shot 0 \
-#   --batch_size 16 \
-#   2>&1 | tee ../log/haerae-deepseek/test-haerae-0shot-nocot-Distill-Llama-8B.log
-
-# python3 -u test_mmlu.py \
-#   --model_name "deepseek-ai/DeepSeek-R1-Distill-Llama-8B" \
-#   --chat_template "deepseek" \
-#   --max_seq_length 512 \
-#   --load_in_4bit \
-#   --single_gpu \
-#   --random_seed 42 \
-#   --task_names haerae_deepseek_cot \
-#   --use_cot \
-#   --n_shot 0 \
-#   --batch_size 16 \
-#   2>&1 | tee ../log/haerae-deepseek/test-haerae-0shot-cot-Distill-Llama-8B.log
-
-# python3 -u test_mmlu.py \
-#   --model_name "deepseek-ai/DeepSeek-R1-Distill-Llama-8B" \
-#   --chat_template "deepseek" \
-#   --max_seq_length 512 \
-#   --load_in_4bit \
-#   --single_gpu \
-#   --random_seed 42 \
-#   --task_names haerae_deepseek \
-#   --no_cot \
-#   --n_shot 8 \
-#   --batch_size 8 \
-#   2>&1 | tee ../log/haerae-deepseek/test-haerae-8shot-nocot-Distill-Llama-8B.log
-
-python3 -u test_mmlu.py \
-  --model_name "deepseek-ai/DeepSeek-R1-Distill-Llama-8B" \
-  --chat_template "deepseek" \
-  --max_seq_length 512 \
-  --load_in_4bit \
-  --single_gpu \
-  --random_seed 42 \
-  --task_names haerae_deepseek_cot \
-  --use_cot \
-  --n_shot 8 \
-  --batch_size 8 \
-  2>&1 | tee ../log/haerae-deepseek/test-haerae-8shot-cot-Distill-Llama-8B.log
-
-python3 -u test_mmlu.py \
-  --model_name "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B" \
-  --chat_template "deepseek" \
-  --max_seq_length 512 \
-  --load_in_4bit \
-  --single_gpu \
-  --random_seed 42 \
-  --task_names haerae_deepseek \
-  --no_cot \
-  --n_shot 0 \
-  --batch_size 16 \
-  2>&1 | tee ../log/haerae-deepseek/test-haerae-0shot-nocot-Distill-Qwen-7B.log
-
-python3 -u test_mmlu.py \
-  --model_name "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B" \
-  --chat_template "deepseek" \
-  --max_seq_length 512 \
-  --load_in_4bit \
-  --single_gpu \
-  --random_seed 42 \
-  --task_names haerae_deepseek_cot \
-  --use_cot \
-  --n_shot 0 \
-  --batch_size 16 \
-  2>&1 | tee ../log/haerae-deepseek/test-haerae-0shot-cot-Distill-Qwen-7B.log
-
-python3 -u test_mmlu.py \
-  --model_name "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B" \
-  --chat_template "deepseek" \
-  --max_seq_length 512 \
-  --load_in_4bit \
-  --single_gpu \
-  --random_seed 42 \
-  --task_names haerae_deepseek \
-  --no_cot \
-  --n_shot 8 \
-  --batch_size 8 \
-  2>&1 | tee ../log/haerae-deepseek/test-haerae-8shot-nocot-Distill-Qwen-7B.log
-
-python3 -u test_mmlu.py \
-  --model_name "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B" \
-  --chat_template "deepseek" \
-  --max_seq_length 512 \
-  --load_in_4bit \
-  --single_gpu \
-  --random_seed 42 \
-  --task_names haerae_deepseek_cot \
-  --use_cot \
-  --n_shot 8 \
-  --batch_size 8 \
-  2>&1 | tee ../log/haerae-deepseek/test-haerae-8shot-cot-Distill-Qwen-7B.log
-
-
-python3 -u test_mmlu.py \
-  --model_name "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B" \
-  --chat_template "deepseek" \
-  --max_seq_length 512 \
-  --load_in_4bit \
-  --single_gpu \
-  --random_seed 42 \
-  --task_names haerae_deepseek \
+  --dataset gsm8k \
+  --subset main \
+  --split test \
   --no_cot \
   --n_shot 0 \
   --batch_size 8 \
-  2>&1 | tee ../log/haerae-deepseek/test-haerae-0shot-nocot-Distill-Qwen-14B.log
+  2>&1 | tee ../log/gsm8k-deepseek/test-gsm8k-0shot-nocot-Distill-Qwen-32B.log
 
-python3 -u test_mmlu.py \
-  --model_name "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B" \
+python3 -u test_gsm.py \
+  --model_name "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B" \
   --chat_template "deepseek" \
   --max_seq_length 512 \
   --load_in_4bit \
-  --single_gpu \
   --random_seed 42 \
-  --task_names haerae_deepseek_cot \
+  --dataset gsm8k \
+  --subset main \
+  --split test \
   --use_cot \
   --n_shot 0 \
   --batch_size 8 \
-  2>&1 | tee ../log/haerae-deepseek/test-haerae-0shot-cot-Distill-Qwen-14B.log
+  2>&1 | tee ../log/gsm8k-deepseek/test-gsm8k-0shot-cot-Distill-Qwen-32B.log
 
-python3 -u test_mmlu.py \
-  --model_name "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B" \
+python3 -u test_gsm.py \
+  --model_name "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B" \
   --chat_template "deepseek" \
   --max_seq_length 512 \
   --load_in_4bit \
-  --single_gpu \
   --random_seed 42 \
-  --task_names haerae_deepseek \
+  --dataset gsm8k \
+  --subset main \
+  --split test \
   --no_cot \
   --n_shot 8 \
-  --batch_size 4 \
-  2>&1 | tee ../log/haerae-deepseek/test-haerae-8shot-nocot-Distill-Qwen-14B.log
+  --batch_size 2 \
+  2>&1 | tee ../log/gsm8k-deepseek/test-gsm8k-8shot-nocot-Distill-Qwen-32B.log
 
-python3 -u test_mmlu.py \
-  --model_name "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B" \
+python3 -u test_gsm.py \
+  --model_name "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B" \
   --chat_template "deepseek" \
   --max_seq_length 512 \
   --load_in_4bit \
-  --single_gpu \
   --random_seed 42 \
-  --task_names haerae_deepseek_cot \
+  --dataset gsm8k \
+  --subset main \
+  --split test \
   --use_cot \
   --n_shot 8 \
-  --batch_size 4 \
-  2>&1 | tee ../log/haerae-deepseek/test-haerae-8shot-cot-Distill-Qwen-14B.log
+  --batch_size 2 \
+  2>&1 | tee ../log/gsm8k-deepseek/test-gsm8k-8shot-cot-Distill-Qwen-32B.log
