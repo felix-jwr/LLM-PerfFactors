f698aab3c4b81dec3ece9b03163fb13d8b946d92
felix-jwr <felix.rammell@outlook.com>
2025-04-27 07:52:55 +0000

    Reset Dockerfile to def, remove mmlu from run2, re-enabled 4bit on test_gsm

--- docker/run2.sh
+++ docker/run2.sh
@@ -16,16 +16,3 @@ python3 -u test_gsm.py \
   --n_shot 0 \
   --batch_size 8 \
   2>&1 | tee ../log/test-gsm8k-0shot-nocot-Llama-3.1-8B-Instruct-NON-4BIT.log
-
-python3 -u test_mmlu.py \
-  --model_name "meta-llama/meta-llama/Llama-3.1-8B-Instruct" \
-  --chat_template "llama" \
-  --max_seq_length 512 \
-  --load_in_4bit \
-  --single_gpu \
-  --random_seed 900 \
-  --task_names haerae \
-  --no_cot \
-  --n_shot 0 \
-  --batch_size 32 \
-  2>&1 | tee ../log/test-haerae-0shot-nocot-Llama-3.1-8B-Instruct-NON-4BIT.log
